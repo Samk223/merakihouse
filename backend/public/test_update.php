4dd72cc88e4cfaccae0357ed7bfad4f6fcc438f2
Somya Kashyap <studytimek21@gmail.com>
2026-06-29 00:43:39 +0530

    clean: delete public/test_update.php

--- backend/public/test_update.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-foreach (App\Models\Product::all() as $p) {
-    echo "ID: " . $p->id . " | Name: " . $p->name . " | Slug: " . $p->slug . "\n";
-}
